b5fcafd1e50035c0fa5ff7294d85b1ffbfc5480c
anuragshukla07 <anuragshukla2k12@gmail.com>
2021-06-17 22:25:36 +0530

    --initial_commit

---
+++ flaskblog.py
@@ -0,0 +1,27 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+posts=[ 
+        {
+        'author':'Anurag Shukla',
+        'title' : 'Post name 1',
+        'content' : 'First post content',
+        'date_posted' : 'June 15 2021'
+        },
+        {
+        'author':'Sameer Ahmed',
+        'title' : 'Post name 2',
+        'content' : 'Second post content',
+        'date_posted' : 'June 17 2021'
+        }
+      ]
+
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
