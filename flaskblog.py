d2baca687d38eed70657ea55f44a2f3a2a040089
anuragshukla07 <anuragshukla2k12@gmail.com>
2021-06-20 00:31:33 +0530

    --add:forms_page

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask , render_template, url_for
+from forms import RegistrationForm , LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '00caf1ee75cf656a5e24c2c36fc57d80'
 
 posts=[ 
         {
@@ -27,6 +30,14 @@ def home():
 def about():
     return render_template('about.html',title='About')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register', form=form)
+
+
+
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html',title='Login', form=form)
