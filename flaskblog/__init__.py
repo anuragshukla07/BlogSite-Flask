cbee21fe4d0c203ccc9214111ae3f26a7070a0db
anuragshukla07 <anuragshukla2k12@gmail.com>
2021-06-24 01:41:02 +0530

    --add:validation_correction

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy  
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '00caf1ee75cf656a5e24c2c36fc57d80'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
